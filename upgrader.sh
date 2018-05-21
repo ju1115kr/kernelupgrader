d07708660b721ca97121c3311586ceac1656805c
Lee Ju Young <ju1115kr@naver.com>
2018-05-21 14:04:48 +0900

    adjust to CentOS 7

--- upgrader.sh
+++ upgrader.sh
@@ -40,23 +40,26 @@ nslookup cdn.kernel.org
 nslookup www.kernel.org
 
 # install preparities packages
-apt-mark hold linux-image-generic linux-headers-generic
-apt-get install -y make bc libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev wget python3 python3-pip
-pip3 install lxml BeautifulSoup4 requests # install python lib
+yum install wget gcc make ncurses-devel openssl-devel elfutils-libelf-devel bc perl kernel-devel -y
+pip install lxml BeautifulSoup4 requests # install python lib
 
 # get latest kernel version from kernel.org
-latest_kernel_version="$(python3 kernel_crawing/kernel_version.py)"
-latest_kernel_link="$(python3 kernel_crawing/kernel_link.py)"
+latest_kernel_version="$(python kernel_crawing/kernel_version.py)"
+latest_kernel_link="$(python kernel_crawing/kernel_link.py)"
 
 wget $latest_kernel_link 	# download latest version kernel file
 tar -xvf linux-$latest_kernel_version.tar.xz && cd linux-$latest_kernel_version # Decompress tar.xz file and change directory
 
 yes "" | make oldconfig		# make oldconfig with all 'yes' command repeatedly output a line with all specified string by default
-make 				# 'make' processing will be VERY VERY LONG TIME :)
+make -j4  				# 'make' processing will be VERY VERY LONG TIME :)
+make -j4 modules
+make modules_install
 make install			# install to system
 
-grub-set-default 0		# change boot sequence
-update-grub			# apply changed grub config
+grub2-set-default 0		# change boot sequence
+grub2-mkconfig -o /boot/grub2/grub.cfg
 
 init 6				# reboot
+
 uname -a			# if you complete reboot please check out kernel version on this system
+package-cleanup --oldkernels --count=1
